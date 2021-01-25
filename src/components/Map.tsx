030cf1c3457362ec52264b7bef661513f01ae5d6
AG <champtoday@gmail.com>
2021-01-25 12:12:24 -0500

    use suspense

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { GeoJSONLayer } from 'react-mapbox-gl';
-import Map from '../util/ReactMapBoxGl'
+import { workerSet } from '../util/ReactMapBoxGl';
 import MapboxGl from 'mapbox-gl';
 import { useData } from '../hooks/useData';
 
@@ -10,49 +10,56 @@ interface MapContainerProps {
   year: string;
 }
 
+//@ts-ignore
+const Map = React.lazy(() =>
+  workerSet().then(() => import('../util/ReactMapBoxGl')),
+);
+
 const MapContainer = ({ year }: MapContainerProps) => {
   const [isLoading, data] = useData(year);
 
   return (
     <div className="map-grid">
-      <Map
-        className="map"
-        style="mapbox://styles/nrgapple/ckk7nff4z0jzj17pitiuejlvt"
-        onStyleLoad={(map: MapboxGl.Map) => {
-          map.setZoom(2);
-          map.resize();
-        }}
-      >
-        {data && (
-          <>
-            <GeoJSONLayer
-              data={data.borders}
-              fillPaint={{
-                'fill-color': ['get', 'COLOR'],
-                'fill-opacity': 0.5,
-              }}
-            />
-            <GeoJSONLayer
-              data={data.labels}
-              symbolLayout={{
-                'text-field': '{NAME}',
-                'text-font': ['Lato Bold'],
-                'text-size': {
-                  base: 1,
-                  stops: [
-                    [12, 12],
-                    [16, 16],
-                  ],
-                },
-                'text-padding': 3,
-                'text-letter-spacing': 0.1,
-                'text-max-width': 7,
-                'text-transform': 'uppercase',
-              }}
-            />
-          </>
-        )}
-      </Map>
+      <Suspense fallback={<div></div>}>
+        <Map
+          className="map"
+          style="mapbox://styles/nrgapple/ckk7nff4z0jzj17pitiuejlvt"
+          onStyleLoad={(map: MapboxGl.Map) => {
+            map.setZoom(2);
+            map.resize();
+          }}
+        >
+          {data && (
+            <>
+              <GeoJSONLayer
+                data={data.borders}
+                fillPaint={{
+                  'fill-color': ['get', 'COLOR'],
+                  'fill-opacity': 0.5,
+                }}
+              />
+              <GeoJSONLayer
+                data={data.labels}
+                symbolLayout={{
+                  'text-field': '{NAME}',
+                  'text-font': ['Lato Bold'],
+                  'text-size': {
+                    base: 1,
+                    stops: [
+                      [12, 12],
+                      [16, 16],
+                    ],
+                  },
+                  'text-padding': 3,
+                  'text-letter-spacing': 0.1,
+                  'text-max-width': 7,
+                  'text-transform': 'uppercase',
+                }}
+              />
+            </>
+          )}
+        </Map>
+      </Suspense>
     </div>
   );
 };
--- src/util/ReactMapBoxGl.ts
+++ src/util/ReactMapBoxGl.ts
@@ -1,11 +1,22 @@
-import "mapbox-gl/dist/mapbox-gl.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 //@ts-ignore
 import mapboxgl from 'mapbox-gl';
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl from 'react-mapbox-gl';
 
-//@ts-ignore
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+export const workerSet = async () => {
+  try {
+    const worker = await import(
+      //@ts-ignore
+      'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'
+    );
+    console.log('here');
+    //@ts-ignore
+    // eslint-disable-next-line import/no-webpack-loader-syntax
+    mapboxgl.workerClass = worker.default;
+  } catch {
+    console.log('In a sandbox');
+  }
+};
 /**
  * Configuration values for Mapbox Component.
  *
@@ -13,7 +24,8 @@ mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worke
  */
 
 export default ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoibnJnYXBwbGUiLCJhIjoiY2trN2E1YnVvMGJ4OTJwbWptM25waHVmNyJ9.UxvOXdAatpV-H1AXQQ23Kg',
+  accessToken:
+    'pk.eyJ1IjoibnJnYXBwbGUiLCJhIjoiY2trN2E1YnVvMGJ4OTJwbWptM25waHVmNyJ9.UxvOXdAatpV-H1AXQQ23Kg',
   minZoom: 1,
   maxZoom: 15,
 });
