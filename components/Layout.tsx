217cdcec89f1443d2cbeae21862c82c0ab512072
AG <champtoday@gmail.com>
2024-12-19 17:53:38 -0500

    ✨ update icon paths in manifest and layout for improved PWA support

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -63,21 +63,21 @@ export default function Layout({
         />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" href="/icons/ios/57.png" />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
+          href="/icons/ios/152.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
+          href="/icons/ios/180.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
+          href="/icons/ios/167.png"
         />
         <link rel="manifest" href="/manifest.json" />
         <meta name="twitter:url" content="https://historicborders.app" />
